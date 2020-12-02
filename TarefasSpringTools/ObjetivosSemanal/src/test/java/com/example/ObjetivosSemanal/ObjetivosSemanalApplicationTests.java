0e1c79801eda28b86b237c22b90f29c2b31d3d21
Mr-Sena <jsenna985@gmail.com>
2020-12-01 22:19:01 -0300

    Full Stack

---
+++ TarefasSpringTools/ObjetivosSemanal/src/test/java/com/example/ObjetivosSemanal/ObjetivosSemanalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ObjetivosSemanal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObjetivosSemanalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
