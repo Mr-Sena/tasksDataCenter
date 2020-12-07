ceb90888ac1f6650ecb8cd9bc6677cd1cc51a3e2
Mr-Sena <jsenna985@gmail.com>
2020-12-07 17:55:47 -0300

    Chave estrangeira no Spring Tools

--- PoweredAPI/blogPessoal/src/main/java/org/generation/blogPessoal/model/Postagem.java
+++ PoweredAPI/blogPessoal/src/main/java/org/generation/blogPessoal/model/Postagem.java
@@ -6,11 +6,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "postagem")
@@ -30,6 +33,10 @@ public class Postagem {
 	
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date datacao = new java.sql.Date(System.currentTimeMillis());
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Tema tema;
 	
 	
 	//Getters And Setters
@@ -56,6 +63,13 @@ public class Postagem {
 	}
 	public void setData(Date datacao) {
 		this.datacao = datacao;
+	}
+	
+	public Tema getTema() {
+		return tema;
+	}
+	public void setTema(Tema tema) {
+		this.tema = tema;
 	}
 	
 	
